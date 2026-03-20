f82ad263baa15c5b59d9e4c152470613ac97f18e
Denis C de Azevedo <denis.azevedo@gmail.com>
2026-03-20 15:36:24 -0300

    chore: bump version

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -90,7 +90,7 @@ export function serverURLFromOptions(options: SDKOptions): URL | null {
 export const SDK_METADATA = {
   language: "typescript",
   openapiDocVersion: "0.1.0",
-  sdkVersion: "0.4.0",
+  sdkVersion: "0.4.1",
   genVersion: "2.869.3",
-  userAgent: "speakeasy-sdk/typescript 0.4.0 2.869.3 0.1.0 fluidapi",
+  userAgent: "speakeasy-sdk/typescript 0.4.1 2.869.3 0.1.0 fluidapi",
 } as const;
